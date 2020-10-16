fc7cc824919787c2977d9ab46adbbe9bb2515845
渣臻 <qz519189636@qq.com>
2020-10-16 11:33:28 +0800

    qz commit

---
+++ src/components/Menu/menu.tsx
@@ -0,0 +1,78 @@
+import React, { FC, CSSProperties, createContext } from "react";
+import classNames from "classnames";
+import { useState } from "@storybook/addons";
+import { IgnorePlugin } from "webpack";
+
+type MenuMode = "horizontal" | "vertical";
+
+export interface MenuProps {
+  defaultIndex?: string;
+  className?: string;
+  // 菜单类型：横向 or 纵向
+  mode?: MenuMode;
+  style?: CSSProperties;
+  onSelect?: (selectedIndex: string) => void;
+  defaultOpenSubMenus?: string[];
+}
+
+interface IMenuContext {
+  index: string;
+  onSelect?: (selectedIndex: string) => void;
+  mode?: MenuMode;
+  defaultOpenSubMenus?: string[];
+}
+
+export const MenuContext = createContext<IMenuContext>({ index: "0" });
+
+export const Menu: FC<MenuProps> = (props) => {
+  const {
+    className,
+    mode,
+    style,
+    children,
+    defaultIndex,
+    onSelect,
+    defaultOpenSubMenus,
+  } = props;
+  const [currentActive, setActive] = useState(defaultIndex);
+
+  const classes = classNames("viking-menu", className, {
+    "menu-vertical": mode === "vertical",
+    "menu-horizontal": mode !== "vertical",
+  });
+
+  const handleClick = (index:string) => {
+    setActive(index)
+    if(onSelect){
+      onSelect(index)
+    }
+  };
+
+  const passedContext: IMenuContext = {
+    index: currentActive ? currentActive : "0",
+    onSelect: handleClick,
+    mode,
+    defaultOpenSubMenus,
+  };
+
+  const renderChildren = () =>{
+    return React.Children.map(children,(child,index)=>{
+      const childElement = child
+      // const {dis}
+    })
+  }
+
+  return (
+    <ul className={classes} style={style} data-testid="test-menu">
+      <MenuContext.Provider value={passedContext}>
+        {renderChildren()}
+      </MenuContext.Provider>
+    </ul>
+  );
+};
+
+Menu.defaultProps = {
+  defaultIndex: "0",
+  mode: "horizontal",
+  defaultOpenSubMenus: [],
+};
