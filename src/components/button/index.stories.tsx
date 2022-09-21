032a5130ba045c2cf132d9730d42ca19540cf14c
qinzhen <qz519189636@qq.com>
2022-09-21 18:14:48 +0800

    feat: build

---
+++ src/components/button/index.stories.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Meta } from "@storybook/react";
+
+const Button = ({children}) => {
+  return <button>Button
+    {children}
+  </button>;
+};
+
+
+const meta: Meta = {
+  title: "Components/Button",
+  component: Button,
+};
+
+type DocsProps = {
+  primary: string;
+  secondary: string;
+  ghost: string;
+  danger: string;
+};
+
+export const Docs = ({ primary, secondary, ghost, danger }: DocsProps) => (
+  <>
+    <Button>1111</Button>
+  </>
+);
+
+// Docs.args = {
+//   primary: "确定",
+//   secondary: "取消",
+//   ghost: "ghost",
+//   danger: "danger",
+// };
+
+export default meta;
